5d9bef3475cf2ecec28ecda259ca5bd9c57900c1
Manjil shrestha <66623411+itsmanjil@users.noreply.github.com>
2022-01-31 16:01:58 +0545

    del
    del

--- README.md
+++
@@ -1,2 +0,0 @@
-# Communicity-Health-management.
-We are building real world problem solving system.
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django import urls
-from . import views
-urlspattern = [
-    path('home', views.home )
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
