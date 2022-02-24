cdd478583d36fad0b968be184e7e8dd460878f1d
Manjil shrestha <66623411+itsmanjil@users.noreply.github.com>
2022-02-24 23:24:36 +0545

    del

--- bookings/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from bookings.models import Bookings
-
-admin.site.register(Bookings)
--- bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from bookings import views
-
-urlpatterns = [
-    path('book', views.book, name = "book")
-
-]
